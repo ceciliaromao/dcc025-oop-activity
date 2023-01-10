01f884b75af63e32afcaf1ea07aae3557d099751
Fernando Giardini <fernandogiardining@outlook.com>
2023-01-10 18:28:15 -0300

    fix/ limpando o que restava tirar do Create

--- agendamentoConsultas/src/main/java/com/mycompany/agendamentoconsultas/controller/CreateUser.java
+++ agendamentoConsultas/src/main/java/com/mycompany/agendamentoconsultas/controller/CreateUser.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 import java.util.ArrayList;
+import javax.swing.JList;
 
 /**
  *
@@ -68,6 +69,6 @@ public class CreateUser implements ActionListener {
         } catch (UserRegistrationException ex) {
             System.out.println("Algo deu errado ao criar o usuário: " + ex.getMessage());
         }
-
+       
     }
 }

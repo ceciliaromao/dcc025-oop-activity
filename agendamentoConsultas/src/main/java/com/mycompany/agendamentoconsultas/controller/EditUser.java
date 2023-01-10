db760b9be39e88afabd5dd1a1d41e02b00692824
Fernando Giardini <fernandogiardining@outlook.com>
2023-01-10 13:32:59 -0300

    Começo da implementação do read do usuário

--- agendamentoConsultas/src/main/java/com/mycompany/agendamentoconsultas/controller/EditUser.java
+++ agendamentoConsultas/src/main/java/com/mycompany/agendamentoconsultas/controller/EditUser.java
@@ -4,9 +4,16 @@
  */
 package com.mycompany.agendamentoconsultas.controller;
 
+import com.mycompany.agendamentoconsultas.model.Admin;
+import com.mycompany.agendamentoconsultas.model.Doctor;
+import com.mycompany.agendamentoconsultas.model.Pacient;
+import com.mycompany.agendamentoconsultas.model.Person;
+import com.mycompany.agendamentoconsultas.model.UserRegistrationException;
 import com.mycompany.agendamentoconsultas.view.UserScreenView;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
+import javax.swing.DefaultListModel;
 
 
 /**
@@ -24,6 +31,51 @@ public class EditUser implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+         DefaultListModel<Person> model = (DefaultListModel<Person>) screenView.getUserList().getModel();
+        
+        try{
+         
+            String document = screenView.getJtDocument().getText();
+                    
+            switch (document.length())  {
+                case 11:
+                    
+                    
+                    
+                    //TODO  
+                    
+                    
+                    
+                    
+                    model.addElement(new Pacient(document,
+                            screenView.getJtName().getText(),
+                            screenView.getJtPhoneNumber().getText(),
+                            screenView.getJtEmail().getText(),
+                            Arrays.toString(screenView.getJPasswordField().getPassword())));
+                    break;
+                case 8:
+                    model.addElement(new Doctor(document,
+                            screenView.getJtName().getText(),
+                            screenView.getJtPhoneNumber().getText(),
+                            screenView.getJtEmail().getText(),
+                            Arrays.toString(screenView.getJPasswordField().getPassword())));
+                    break;
+                case 5:
+                    model.addElement(new Admin(document,
+                            screenView.getJtName().getText(),
+                            screenView.getJtPhoneNumber().getText(),
+                            screenView.getJtEmail().getText(),
+                            Arrays.toString(screenView.getJPasswordField().getPassword())));
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+                    
+        }catch(UserRegistrationException ex){
+            System.out.println("Erro na criação de usuário: "+ ex.getMessage());
+        }
+        
+        screenView.getUserList().setModel(model);
+        screenView.repaint();
     }
 }
